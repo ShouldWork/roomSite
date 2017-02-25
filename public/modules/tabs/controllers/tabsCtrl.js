e39da115ac02c711f51e5ef9095436ff30d73886
Blake Locke <I54626@xactware.com>
2017-02-24 21:01:15 -0700

    updated posts page and moved the FAB button to the top right

--- public/modules/tabs/controllers/tabsCtrl.js
+++ public/modules/tabs/controllers/tabsCtrl.js
@@ -1,10 +1,9 @@
 angular.module('tabsCtrl', ['tabsService','postService'])
-.controller('tabsCtrl', function(Tabs, Posts) {
+.controller('tabsCtrl', function(Tabs, Posts, $mdDialog,$scope, toastService) {
 	
 	vm = this;
 
-    vm.toTest = "Something to test";
-
+    var toast = toastService; 
 	// Grab all the items from Tabs Service
 	Tabs.all()
     .then(function(data){
@@ -15,7 +14,7 @@ angular.module('tabsCtrl', ['tabsService','postService'])
 
     Posts.all()
     .then(function(data){
-        vm.posts = data.data.list;
+        vm.posts = data.data.list; 
     }, function(error){
         console.log("Something went wrong..." + error); 
     }); 
@@ -24,6 +23,48 @@ angular.module('tabsCtrl', ['tabsService','postService'])
         var currentPosts = vm.posts; 
         // var currentPosts.push(post);
     }
+        //     <md-input-container class="md-block">
+        //   <label>Biography</label>
+        //   <textarea ng-model="user.biography" md-maxlength="150" rows="5" md-select-on-focus></textare<a>
+        // </md-input-container>
+     vm.showNewFormDialogue = function(ev) {
+        console.log('Clicked the button');
+          $mdDialog.show({
+            controller: newFormDialogue,
+            template: '<div layout-padding layout="column" layout-align="center center"></md-input-container><md-input-container style="align-self: flex-start"> <input type="text" ng-model="newFormEntry.title" placeholder="Title..." /> </md-input-container><md-input-container style="min-width: 250px" class="md-block"><label>Post Body..</label><textarea ng-model="newFormEntry.body" rows="4" md-select-on-focus></textarea></md-input-container></div><div stlyle="align-self: flex-end" layout="row" layout-align="center center"><md-button ng-click="create(newFormEntry)" class="md-button md-primary">Post</md-button><md-button ng-click="cancel()" class="md-button md-warn">Cancel</md-button>' ,
+            parent: angular.element(document.bodystyle),
+            targetEvent: ev,
+            clickOutsideToClose:true,
+            fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
+          })
+          .then(function(answer) {
+            // console.log("user name: " + answer.displayName  + " email: " + answer.emailAddress + " password: " + answer.loginPassword);
+            $scope.newFormEntry = {
+                title: answer.title,
+                body: answer.body,
+                author: answer.author
+            }
+            vm.posts.push($scope.newFormEntry);
+            console.log(vm.posts);
+            toast.showSimpleToast($scope.newFormEntry.title+' -- Posted!');
+          }, function(){
+            toast.showSimpleToast('Post abandoned!');
+        });
+      };
+
+      function newFormDialogue($scope, $mdDialog){
+            $scope.hide = function() {
+              $mdDialog.hide();
+            };
+
+            $scope.cancel = function() {
+              $mdDialog.cancel();
+            };
+
+            $scope.create = function(answer) {
+              $mdDialog.hide(answer);
+            };
+     };
 
 })
 .directive('posts', function(){
